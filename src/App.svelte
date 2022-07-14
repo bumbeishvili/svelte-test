1698aec870381ff954b71d9bf339697e3fc40b44
bumbeishvili <me@davidb.dev>
2022-07-14 17:57:56 +0400

    initial files

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+ import Modal from "./Modal.svelte";
+ let people = [
+  {name: "yoshi", beltColor: "black", age: 25, id: 1},
+  {name: "mario", beltColor: "orange", age: 45, id: 2},
+  {name: "luigi", beltColor: "brown", age: 35, id: 3},
+ ];
+ const handleClick = (person) => {
+  people = people.filter((d) => d != person);
+ };
+ const num = 3;
+</script>
+
+<Modal />
+
+<main>
+ {#each people as person (person.id)}
+  <div>
+   <h4>{person.name}</h4>
+   {#if person.beltColor === "black"}
+    <p><strong>master ninja</strong></p>
+   {/if}
+   <p>{person.age} years old {person.beltColor} belt</p>
+   <button on:click={(event) => handleClick(person)}> delete</button>
+  </div>
+ {:else}
+  <p>No people</p>
+ {/each}
+</main>
+
+<style>
+ main {
+  text-align: center;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+ }
+
+ h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+ }
+
+ @media (min-width: 640px) {
+  main {
+   max-width: none;
+  }
+ }
+</style>
